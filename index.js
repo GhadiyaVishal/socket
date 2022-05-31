ad7f84284b15310958560f2c3e33485d7e092376
Vishal Ghadiya <vishalghadiya29@gmail.com>
2022-05-31 16:29:45 +0530

    second commit

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require("express")
 const app = express();
 const http = require("http")
 const server = http.createServer(app);
-
+const { Server } = require("socket.io")
+const io = new Server(server)
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/index.html")
+})
+
+
+//for connection
+io.on("connection", (socket) => {
+    console.log("a user conected");
+    socket.on("disconnect" , () => {
+        console.log("a user disconnected");
+    })
+
+    socket.on("chat message" , (msg) => {
+        io.emit("chat message", msg);
+    })
 })
 
 server.listen(5000, () =>{
